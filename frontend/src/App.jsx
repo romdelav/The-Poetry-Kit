bb5973699ac617f3de20c671a288729913fc3217
Romain <rom.delav@gmail.com>
2021-05-14 10:36:59 -0500

    implemented navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import NavBar from './components/navbar';
+import NotFound from './components/notfound';
 
 function App() {
     return ( 
-        <div>
-        Hello World!
-        </div>
+        <Router >
+            <div>
+                <NavBar />
+                <div id="page-body">
+                    <Switch>
+                        <Route path="/" component={Home} exact />
+                        <Route path="/haikus"/>
+                        <Route path="/exquisite-corpses" />
+                        <Route path="/constrained-poems" />
+                        <Route component={NotFound} />
+                    </Switch>
+                    
+                </div>
+            </div> 
+        </Router>
     );
 }
 
