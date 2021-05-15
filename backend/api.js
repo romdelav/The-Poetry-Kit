78aa1f9e93106dcc361903f41dfd7798735e70de
Romain <rom.delav@gmail.com>
2021-05-15 16:19:47 -0500

    updated api request

--- backend/api.js
+++ backend/api.js
@@ -23,6 +23,6 @@ app.route('/constrained-poems')
     );
 
 function getRandomConstraint() {
-    const randomConstraint = db.prepare('SELECT * FROM Constraint').all();
+    const randomConstraint = db.prepare('SELECT * FROM Constraint ORDER BY RANDOM() LIMIT 1').all();
     return randomConstraint;
 }
