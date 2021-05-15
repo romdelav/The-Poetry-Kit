3446a9561ccdd0a19a80cb3e5b5c93d7c4f2750f
Romain <rom.delav@gmail.com>
2021-05-15 09:52:52 -0500

    set up API

--- backend/api.js
+++ backend/api.js
@@ -1,8 +1,17 @@
-const express = require("express");
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+
+const dbConnection = path.join(__dirname + '/database/Poetry_Kit.db');
+console.log(dbConnection);
+const db = require('better-sqlite3')(dbConnection, { verbose: console.log });
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
