821738766876be24cea9a9aeb6ea1bf2f94792ce
Romain <rom.delav@gmail.com>
2021-05-17 10:59:41 -0500

    updated navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -11,15 +11,25 @@ const NavBar = () => (
                 <button class="dropdown-button">Haikus</button>
                 <div class="dropdown-content">
                     <Link to="/haikus/history">History</Link>
-                    <a href="#">Popular Haikus</a>
-                    <a href="#">Create a Haiku</a>
+                    <Link to="/haikus/popular-haikus">Popular Haikus</Link>
+                    <Link to="/haikus/create-haiku">Create a Haiku</Link>
                 </div>
             </li>
-            <li>
-                <Link to="/exquisite-corpses">Exquisite Corpses</Link>
+            <li class="dropdown">
+                <button class="dropdown-button">Exquisite Corpses</button>
+                <div class="dropdown-content">
+                    <Link to="/exquisite-corpses/history">History</Link>
+                    <Link to="/exquisite-corpses/popular-exquisite-corpses">Popular Exquisite Corpses</Link>
+                    <Link to="/exquisite-corpses/create-exquisite-corpse">Create an Exquisite Corpse</Link>
+                </div>
             </li>
-            <li>
-                <Link to="/constrained-poems">Constrained Poems</Link>
+            <li class="dropdown">
+            <button class="dropdown-button">Constrained Poems</button>
+            <div class="dropdown-content">
+                    <Link to="/constrained-poems/history">History</Link>
+                    <Link to="/constrained-poems/popular-constrained-poems">Popular Exquisite Corpses</Link>
+                    <Link to="/constrained-poems/create-constrained-poem">Create an Exquisite Corpse</Link>
+                </div>
             </li>
         </ul>
     </nav>
