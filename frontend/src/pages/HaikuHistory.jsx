fdf936d0c9f19c384fa2ca1322e72b17e28355de
Romain <rom.delav@gmail.com>
2021-05-17 10:39:34 -0500

    created haiku history page

---
+++ frontend/src/pages/HaikuHistory.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HaikuHistory = () => {
+    return (    
+        <div>
+            <br/><br/><br/><br/>
+            Haiku History
+        </div>
+    )
+};
+
+export default HaikuHistory;
