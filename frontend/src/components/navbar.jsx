0cbedfc020814ea1928edb2810727875290136ca
Romain <rom.delav@gmail.com>
2021-05-17 10:40:27 -0500

    created hoverable dropdown menu

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -7,8 +7,13 @@ const NavBar = () => (
             <li>
                 <Link to="/">Home</Link>
             </li>
-            <li>
-                <Link to="/haikus">Haikus</Link>
+            <li class="dropdown">
+                <button class="dropdown-button">Haikus</button>
+                <div class="dropdown-content">
+                    <Link to="/haikus/history">History</Link>
+                    <a href="#">Popular Haikus</a>
+                    <a href="#">Create a Haiku</a>
+                </div>
             </li>
             <li>
                 <Link to="/exquisite-corpses">Exquisite Corpses</Link>
