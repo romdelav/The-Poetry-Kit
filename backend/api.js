6d93153d66a076d7d259974923e770b382f1a688
Romain <rom.delav@gmail.com>
2021-05-17 00:17:38 -0500

    updated api request to retrieve haiku lines, created api resquest to retrieve haiku themes

--- backend/api.js
+++ backend/api.js
@@ -17,32 +17,42 @@ app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
 
-app.route('/haikus')
+app.route('/create-haiku')
+    .get((req, res) =>
+        res.send(JSON.stringify(getHaikuThemes(), null, 2))
+    );
+
+app.route('/create-haiku/:themeID')
     .get((req, res) =>
         res.send(JSON.stringify(getHaikuLines(), null, 2))
     );
 
-app.route('/constrained-poems')
+app.route('/create-constrained-poem')
     .get((req, res) =>
         res.send(JSON.stringify(getRandomConstraint(), null, 2))
     );
 
-function getRandomConstraint() {
-    const randomConstraint = db.prepare('SELECT * FROM Constraints ORDER BY RANDOM() LIMIT 1').get();
-    return randomConstraint;
+function getHaikuThemes() {
+    const haikuThemes = db.prepare('SELECT * FROM Theme').all();
+    return haikuThemes;
 }
 
-function getHaikuLines() {
+function getHaikuLines(themeID) {
     const haikuLines = { haikuLine1: '', haikuLine2: '', haikuLine3: '' };
 
-    const line1 = db.prepare(`SELECT * FROM HaikuLine WHERE lineNumber = 1 ORDER BY RANDOM() LIMIT 3`).all();
+    const line1 = db.prepare(`SELECT * FROM HaikuLine WHERE themeID = ${themeID} AND lineNumber = 1 ORDER BY RANDOM() LIMIT 3`).all();
     haikuLines.haikuLine1 = line1.haikuLine1;
 
-    const line2 = db.prepare(`SELECT * FROM HaikuLine WHERE lineNumber = 2 ORDER BY RANDOM() LIMIT 3`).all();
+    const line2 = db.prepare(`SELECT * FROM HaikuLine WHERE themeID = ${themeID} AND lineNumber = 2 ORDER BY RANDOM() LIMIT 3`).all();
     haikuLines.haikuLine2 = line2.haikuLine2;
 
-    const line3 = db.prepare(`SELECT * FROM HaikuLine WHERE lineNumber = 3 ORDER BY RANDOM() LIMIT 3`).all();
+    const line3 = db.prepare(`SELECT * FROM HaikuLine WHERE themeID = ${themeID} AND lineNumber = 3 ORDER BY RANDOM() LIMIT 3`).all();
     haikuLines.haikuLine3 = line3.haikuLine3;
 
     return haikuLines;
+}
+
+function getRandomConstraint() {
+    const randomConstraint = db.prepare('SELECT * FROM Constraints ORDER BY RANDOM() LIMIT 1').get();
+    return randomConstraint;
 }
