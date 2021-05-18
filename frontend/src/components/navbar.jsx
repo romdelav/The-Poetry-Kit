d7e4cde39f76b201ef96d8d4e9d8b6a66bef2b3b
Romain <rom.delav@gmail.com>
2021-05-18 10:46:21 -0500

    styled navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -4,31 +4,34 @@ import {Link} from 'react-router-dom';
 const NavBar = () => (
     <nav>
         <ul>
-            <li>
-                <Link to="/">Home</Link>
+            <li class="navbar">
+                <div class="navbar-button">
+                    <Link to="/" style={{ textDecoration: 'none' }}>Home</Link>
+                </div>
+                
             </li>
-            <li class="dropdown">
-                <button class="dropdown-button">Haikus</button>
+            <li class="navbar">
+                <button class="navbar-button">Haikus</button>
                 <div class="dropdown-content">
                     <Link to="/haikus/history">History</Link>
                     <Link to="/haikus/popular">Popular Haikus</Link>
                     <Link to="/haikus/create">Create a Haiku</Link>
                 </div>
             </li>
-            <li class="dropdown">
-                <button class="dropdown-button">Exquisite Corpses</button>
+            <li class="navbar">
+                <button class="navbar-button">Exquisite Corpses</button>
                 <div class="dropdown-content">
                     <Link to="/exquisite-corpses/history">History</Link>
                     <Link to="/exquisite-corpses/popular">Popular Exquisite Corpses</Link>
                     <Link to="/exquisite-corpses/create">Create an Exquisite Corpse</Link>
                 </div>
             </li>
-            <li class="dropdown">
-            <button class="dropdown-button">Constrained Poems</button>
+            <li class="navbar">
+            <button class="navbar-button">Constrained Poems</button>
             <div class="dropdown-content">
                     <Link to="/constrained-poems/history">History</Link>
-                    <Link to="/constrained-poems/popular">Popular Exquisite Corpses</Link>
-                    <Link to="/constrained-poems/create">Create an Exquisite Corpse</Link>
+                    <Link to="/constrained-poems/popular">Popular Constrained Poems</Link>
+                    <Link to="/constrained-poems/create">Create a Constained Poem</Link>
                 </div>
             </li>
         </ul>
