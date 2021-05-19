0843617f1ebf7b80bad408807b39fd9cea3cc015
Romain <rom.delav@gmail.com>
2021-05-18 23:06:42 -0500

    fixed syntax

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -4,31 +4,31 @@ import {Link} from 'react-router-dom';
 const NavBar = () => (
     <nav>
         <ul>
-            <li class="navbar">
-                <div class="navbar-button">
+            <li className="navbar">
+                <div className="navbar-button">
                     <Link to="/" style={{ textDecoration: 'none' }}>Home</Link>
                 </div>
                 
             </li>
-            <li class="navbar">
-                <button class="navbar-button">Haikus</button>
-                <div class="dropdown-content">
+            <li className="navbar">
+                <button className="navbar-button">Haikus</button>
+                <div className="dropdown-content">
                     <Link to="/haikus/history">History</Link>
                     <Link to="/haikus/popular">Popular Haikus</Link>
                     <Link to="/haikus/create">Create a Haiku</Link>
                 </div>
             </li>
-            <li class="navbar">
-                <button class="navbar-button">Exquisite Corpses</button>
-                <div class="dropdown-content">
+            <li className="navbar">
+                <button className="navbar-button">Exquisite Corpses</button>
+                <div className="dropdown-content">
                     <Link to="/exquisite-corpses/history">History</Link>
                     <Link to="/exquisite-corpses/popular">Popular Exquisite Corpses</Link>
                     <Link to="/exquisite-corpses/create">Create an Exquisite Corpse</Link>
                 </div>
             </li>
-            <li class="navbar">
-            <button class="navbar-button">Constrained Poems</button>
-            <div class="dropdown-content">
+            <li className="navbar">
+            <button className="navbar-button">Constrained Poems</button>
+            <div className="dropdown-content">
                     <Link to="/constrained-poems/history">History</Link>
                     <Link to="/constrained-poems/popular">Popular Constrained Poems</Link>
                     <Link to="/constrained-poems/create">Create a Constained Poem</Link>
