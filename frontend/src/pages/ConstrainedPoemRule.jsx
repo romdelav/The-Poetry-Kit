e1561eddfea04b507af2499a8d8d7df25dd18a97
Romain <rom.delav@gmail.com>
2021-05-21 21:51:29 -0500

    added styles

--- frontend/src/pages/ConstrainedPoemRule.jsx
+++ frontend/src/pages/ConstrainedPoemRule.jsx
@@ -20,18 +20,23 @@ const ConstrainedPoemRule = () => {
 
     return(    
         <>
-        <br/><br/><br/><br/>
-
-        <header>
-            <h1>here is your rule:</h1>
-        </header>
-        
-        <br/><br/>
+        <br/><br/><br/><br/><br/><br/>
 
         <section>
+            
             {Object.values(ruleInfo).map((rule, key) =>
-                <div key={key}>{rule}</div>)}
-        </section>     
+            <div key={key} className="rule-style">
+                <h2>Rule</h2>
+                {rule}
+                <br/><br/>
+            </div>)}
+        </section> 
+
+        <br/><br/><br/><br/>
+
+        <section className="input-field">
+            <textarea placeholder="Your poem..."></textarea>
+        </section>    
         </>
     )
 }
