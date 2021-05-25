2138e608b17e7e101650847b39210324e1e9c105
Romain <rom.delav@gmail.com>
2021-05-25 10:43:33 -0500

    connected to API

--- frontend/src/pages/HaikuLine.jsx
+++ frontend/src/pages/HaikuLine.jsx
@@ -4,33 +4,39 @@ const HaikuLine = ({match}) => {
 
     const themeID = match.params.themeID;
     
-    const [haikuLine, setHaikuLine] = useState({haikuLine1: [], haikuLine2: [], haikuLine3: []});
+    const [haikuLine, setHaikuLine] = useState({haikuLine1: [], haikuLine2: [], haikuLine3:[]});
 
     useEffect(() => {
         const fetchData = async () => {
-            const result = await fetch(`http://localhost:4000/haikus/create/${themeID}`);
+            const result = await fetch(`http://localhost:4000/haikus/create/${themeID}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
             const body = await result.json();
             setHaikuLine(body);
         }
         fetchData();
-    }) 
+    }, [themeID]) 
     
     return(   
         <>
-        <br/><br/><br/>
+         <br/><br/><br/>
             <h2>Select the first line</h2>
-            <br/>
-            {haikuLine.haikuline1.map((lines) => 
+            {haikuLine.haikuLine1.map((lines) => 
             <div key={lines.haikuLineID}>
                 <div>{lines.line}</div>
             </div>)} 
             <br/>
-            {haikuLine.haikuline2.map((lines) => 
+            <h2>Select the second line</h2>
+            {haikuLine.haikuLine2.map((lines) => 
             <div key={lines.haikuLineID}>
                 <div>{lines.line}</div>
             </div>)} 
             <br/>
-            {haikuLine.haikuline3.map((lines) => 
+            <h2>Select the third line</h2>
+            {haikuLine.haikuLine3.map((lines) => 
             <div key={lines.haikuLineID}>
                 <div>{lines.line}</div>
             </div>)} 
