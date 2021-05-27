c804ce8f5ffc5a0cd037d4899a64be379b943f18
Romain <rom.delav@gmail.com>
2021-05-27 14:57:05 -0500

    updated haikuHistory component to display haiku history

--- frontend/src/pages/HaikuHistory.jsx
+++ frontend/src/pages/HaikuHistory.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 
 const HaikuHistory = () => {
+
+    const [history, setHistory] = useState([]);
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch('http://localhost:4000/haikus/history', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const body = await result.json();
+            setHistory(body);
+        }
+        fetchData();
+    }, [])
     return (    
         <div>
             <br/><br/><br/><br/>
             Haiku History
+            <br/>
+            {Object.values(history).map((haikuHistory, key) =>
+            <div key={key}>{haikuHistory.paragraph1}</div>)}
         </div>
     )
 };
