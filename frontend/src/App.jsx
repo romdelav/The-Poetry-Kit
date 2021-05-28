bea2ce089d227cdae711addf3b17d58da921fc8d
Romain <rom.delav@gmail.com>
2021-05-28 17:53:04 -0500

    routed ConstrainedPoemHistory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import HaikuHistory from './pages/HaikuHistory';
 import HaikuTheme from './pages/HaikuTheme';
 import HaikuLine from './pages/HaikuLine';
+import ConstrainedPoemHistory from './pages/ConstrainedPoemHistory';
 import ConstrainedPoemRule from './pages/ConstrainedPoemRule';
 import NavBar from './components/navbar';
 import NotFound from './components/notfound';
@@ -21,6 +22,7 @@ function App() {
                         <Route path="/haikus/create/:themeID" component={HaikuLine} />
                         <Route path="/haikus/create" component={HaikuTheme} />
                         <Route path="/exquisite-corpses/history" component={ExquisiteCorpseHistory} />
+                        <Route path="/constrained-poems/history" component={ConstrainedPoemHistory} />
                         <Route path="/constrained-poems/create" component={ConstrainedPoemRule} />
                         <Route component={NotFound} />
                     </Switch>
