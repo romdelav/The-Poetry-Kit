cf9d2311355a9bb9fdfc551d191b401e1e8ca54b
Romain <rom.delav@gmail.com>
2021-05-31 18:20:14 -0500

    built postHaiku component

--- frontend/src/pages/HaikuLine.jsx
+++ frontend/src/pages/HaikuLine.jsx
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from 'react';
 
-const HaikuLine = ({match}) => {
+const HaikuLine = ({match, setPoem}) => {
 
     const themeID = match.params.themeID;
     
@@ -26,7 +26,17 @@ const HaikuLine = ({match}) => {
     const [line3, setLine3] = useState();
 
     const postHaiku = async() => {
-        //TODO
+        const result = await fetch('http://localhost:4000/constrained-poems/create', {
+            method: 'POST',
+            body: JSON.stringify({title, line1, line2, line3}),
+            headers: { 'Content-Type': 'application/json'}
+        });
+        const body = await result.json();
+        setPoem(body);
+        setTitle('');
+        setLine1();
+        setLine2();
+        setLine3();
     }
 
     return(   
