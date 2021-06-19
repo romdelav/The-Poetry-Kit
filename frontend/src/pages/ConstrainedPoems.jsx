f6c32c70bb7f135cfd56db81251f88e0687ad93b
Romain <rom.delav@gmail.com>
2021-06-18 21:03:21 -0500

    renamed ConstrainedPoemByRuleID component and updated it to display rule description

--- frontend/src/pages/ConstrainedPoems.jsx
+++ frontend/src/pages/ConstrainedPoems.jsx
@@ -1,14 +1,12 @@
 import {React, useState, useEffect} from 'react';
 
-const ConstrainedPoemByRuleID = ({match}) => {
-
-    const ruleID = match.params.ruleID;
+const ConstrainedPoems = () => {
 
     const [constrainedPoem, setConstrainedPoem] = useState([]);
     
     useEffect(() => {
         const fetchData = async () => {
-            const result = await fetch(`http://localhost:4000/constrained-poems/rules/${ruleID}`, {
+            const result = await fetch('http://localhost:4000/constrained-poems/', {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json'
@@ -18,14 +16,14 @@ const ConstrainedPoemByRuleID = ({match}) => {
             setConstrainedPoem(body);
         }
         fetchData();
-    }, [ruleID])
+    }, [])
 
     return (    
         <div>
             {constrainedPoem.map((poem, key) =>
-            <div key={key}>{poem.title}<br/>{poem.text}</div>)}
+            <div key={key}>{poem.description}<br/>{poem.title}<br/>{poem.text}</div>)}
         </div>
     )
 };
 
-export default ConstrainedPoemByRuleID;
+export default ConstrainedPoems;
