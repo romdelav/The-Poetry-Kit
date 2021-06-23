226224e8b3c86650439a506a38f851639286fdd5
Romain <rom.delav@gmail.com>
2021-06-22 20:58:59 -0500

    routed HaikuTitle component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,10 @@ import HaikuHistory from './pages/HaikuHistory';
 import HaikuTheme from './pages/HaikuTheme';
 import HaikuLine from './pages/HaikuLine';
 import MyHaiku from './pages/MyHaiku';
+import HaikuTitle from './pages/HaikuTitle';
 import ConstrainedPoemHistory from './pages/ConstrainedPoemHistory';
 import ConstrainedPoemRule from './pages/ConstrainedPoemRule';
-import ConstrainedPoemByRuleID from './pages/ConstrainedPoemByRuleID';
+import ConstrainedPoems from './pages/ConstrainedPoems';
 import ConstrainedPoem from './pages/ConstrainedPoem';
 import NavBar from './components/navbar';
 import NotFound from './components/notfound';
@@ -26,13 +27,14 @@ function App() {
                         <Route path="/haikus/create/:themeID" component={HaikuLine} />
                         <Route path="/haikus/create" component={HaikuTheme} />
                         <Route path="/haikus/history" component={HaikuHistory} />
+                        <Route path="/haikus/titles" component={HaikuTitle} />
                         <Route path="/haikus/my-haiku/:poemID" component={MyHaiku} />
                         <Route path="/exquisite-corpses/history" component={ExquisiteCorpseHistory} />
                         <Route path="/exquisite-corpses/select/:poemID" component={ExquisiteCorpse} />
                         <Route path="/exquisite-corpses/select" component={ExquisiteCorpseSelection} />
                         <Route path="/constrained-poems/history" component={ConstrainedPoemHistory} />
                         <Route path ="/constrained-poems/:poemID" component={ConstrainedPoem} />
-                        <Route path="/constrained-poems" component={ConstrainedPoemByRuleID} />
+                        <Route path="/constrained-poems" component={ConstrainedPoems} />
                         <Route path="/constrained-poems/create" component={ConstrainedPoemRule} />
                         <Route component={NotFound} />
                     </Switch>
