9048d430e473ef56d82dd8f7cfd646c0918eb3b8
Romain <rom.delav@gmail.com>
2021-06-25 21:36:11 -0500

    built Haiku component

---
+++ frontend/src/pages/Haiku.jsx
@@ -0,0 +1,33 @@
+import {React, useState, useEffect} from 'react';
+
+const Haiku = ({match}) => {
+
+    const poemID = match.params.poemID;
+
+    const [haiku, setHaiku] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`http://localhost:4000/haikus/titles/${poemID}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const body = await result.json();
+            setHaiku(body);
+        }
+        fetchData();
+    }, [poemID]) 
+
+    return (
+        <>
+        {haiku.map((poem) =>
+        <div key={poem.haikuLineID}>
+            {poem.line}
+        </div>)}    
+        </>
+    )
+};
+
+export default Haiku;
